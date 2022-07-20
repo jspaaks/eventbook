62e5f54fd674f839ed14e2bf651d2afb23f58a48
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-07-20 10:20:58 -0400

    data is now initialized from main

--- src/main.go
+++ src/main.go
@@ -4,10 +4,22 @@ import (
 	"log"
 	"net/http"
 
-	routing "github.com/jspaaks/eventbook/routing"
+	routing "github.com/jspaaks/eventbook/src/routing"
+	types "github.com/jspaaks/eventbook/src/types"
 )
 
 func main() {
-	var r = routing.CreateNewRouter().AddRoutesAndHandlers()
+
+	var events []types.Event = []types.Event{
+		{
+			Text:     "the first text",
+			MoreText: "More text pertaining to the first item",
+		}, {
+			Text:     "the second text",
+			MoreText: "More text pertaining to the second item",
+		},
+	}
+
+	var r = routing.CreateNewRouter().AddRoutesAndHandlers(&events)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
